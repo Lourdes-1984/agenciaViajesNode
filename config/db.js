a102089cf6ca9fbc64be06b886cb1acf5a752781
lourdes sosa <lourdes1984.sosa@gmail.com>
2023-03-21 16:22:44 -0300

    creacion de variable de entorno

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 import  Sequelize  from "sequelize";
+import dotenv from 'dotenv/config'
 
-const db = new Sequelize('agenciaViajes' , 'root' , 'Ecuador.123', {
-    host: '127.0.0.1',
+console.log(process.env.DB_HOST);
+
+
+const db = new Sequelize(process.env.DB_NAME , process.env.DB_USER , process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     port: '3306',
     dialect: 'mysql',
     define: {
